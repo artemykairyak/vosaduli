56f016d6227aa1165caac5b5ce533ba0a24a2348
artemykairyak <artemykairyak@yandex.com>
2019-09-05 16:31:07 +0400

    05.09.19

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,17 +10,108 @@ $(function(){
 			$(this).siblings().find('.profile-menu__link').removeClass('profile-menu__link_active');
 		}
 		e.preventDefault();
+	});
+
+	$('.myprofile__submenu-item').on('click', function(e) {
+		e.preventDefault();
+		if (!$(this).hasClass('myprofile__submenu-item_active')) {
+			$(this).toggleClass('myprofile__submenu-item_active');
+			$(this).siblings().removeClass('myprofile__submenu-item_active');
+		}
+
+		if($(this).hasClass('myprofile__submenu-item_aboutme')) {
+			$('.myprofile-content').removeClass('myprofile-section_disabled');
+			$('.myprofile-soc').addClass('myprofile-section_disabled');
+			$('.myprofile-contacts').addClass('myprofile-section_disabled')
+		} else if($(this).hasClass('myprofile__submenu-item_soc')) {
+			$('.myprofile-content').addClass('myprofile-section_disabled');
+			$('.myprofile-soc').removeClass('myprofile-section_disabled');
+			$('.myprofile-contacts').addClass('myprofile-section_disabled')
+		} else if($(this).hasClass('myprofile__submenu-item_contacts')) {
+			$('.myprofile-content').addClass('myprofile-section_disabled');
+			$('.myprofile-soc').addClass('myprofile-section_disabled');
+			$('.myprofile-contacts').removeClass('myprofile-section_disabled')
+		}
 	})
 
-	$('.trigger').on('click', function(e) {
-		$(this).toggleClass('trigger_active');
-		$(this).siblings().removeClass('trigger_active');
+	$('.groups-menu__trigger').on('click', function(e) {
 		e.preventDefault();
+		onTriggerClick($(this));
+
+		if($(this).hasClass('groups-menu__trigger_new')) {
+			$('.groups-menu__new-items').removeClass('groups-menu__items_disabled');
+			$('.groups-menu__best-items').addClass('groups-menu__items_disabled');
+		} else if ($(this).hasClass('groups-menu__trigger_best')){
+			$('.groups-menu__best-items').removeClass('groups-menu__items_disabled');
+			$('.groups-menu__new-items').addClass('groups-menu__items_disabled');
+		}	
+	});
+
+	$('.online-report__trigger').on('click', function(e) {
+		e.preventDefault();
+		onTriggerClick($(this));
+
+		if($(this).hasClass('online-report__trigger_events')) {
+			$('.online-report__events-items').removeClass('online-report__items_disabled');
+			$('.online-report__news-items').addClass('online-report__items_disabled');
+		} else if ($(this).hasClass('online-report__trigger_news')){
+			$('.online-report__news-items').removeClass('online-report__items_disabled');
+			$('.online-report__events-items').addClass('online-report__items_disabled');
+		}	
 	});
 
 	$('.friends__menu-item').on('click', function(e) {
 		$(this).addClass('friends__menu-item_active');
 		$(this).siblings().removeClass('friends__menu-item_active');
 		e.preventDefault();
+	});
+
+	$('select[name="country"]').on('change', function (e) {
+		$('select[name="region"]').removeAttr('disabled').parent().removeClass('edit-select_disabled');
+	});
+
+	$('select[name="region"]').on('change', function (e) {
+		$('select[name="city"]').removeAttr('disabled').parent().removeClass('edit-select_disabled');
+	});
+
+	$(window).on('resize', function() {
+
 	})
+
+	
+	if($(window).width() < 1050) {
+		$('.header-menu__container').owlCarousel({
+			// navText: [$('.header-menu__nav-arrow_prev'), $('.header-menu__nav-arrow_next')],
+			items: 4,
+			margin: 10,
+			nav: false,
+			dots: false,
+			loop: false,
+			autoWidth: true
+			// responsive: {
+			// 	0: {
+			// 		items: 1.3,
+			// 		margin: 30
+			// 	},
+			// 	834: {
+			// 		items: 2,
+			// 		margin: 30
+			// 	},
+
+			// 	1240: {
+			// 		items: 3,
+			// 		margin: 30
+			// 	}
+			// }
+
+		});
+	}
+	
 })
+
+function onTriggerClick(trigger) {
+	if (!trigger.hasClass('trigger_active')) {
+		trigger.toggleClass('trigger_active');
+		trigger.siblings().removeClass('trigger_active');
+	}
+}
