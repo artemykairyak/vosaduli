b51d65f25dd532d1e3f2698ad20e968db2f19e16
Artemy Kairyak <artemykairyak@yandex.com>
2019-10-14 23:31:04 +0400

    14.10.19

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -384,14 +384,14 @@ $(function() {
     // ADD ARTICLE
 
     setTimeout(function() {
-        if ($('.pub-maininfo__selected-categories').length > 0) {         
+        if ($('.pub-maininfo__selected-categories').length > 0) {
             $('.pub-maininfo__category-input_main-category option').each((i, item1) => {
                 $('.pub-maininfo__category-secoundary-inputs-container').find('input[type="hidden"]').each((j, item2) => {
                     if ($(item1).val() == $(item2).val()) {
                         $('.pub-maininfo__category-input_additional-category li').each((k, item3) => {
                             if ($(item1).text() == $(item3).text()) {
-                               $(item3).addClass('category_selected');
-                               $('.pub-maininfo__selected-categories').append('<span class="pub-maininfo__selected-category">' + $(item1).text() + '</span>')
+                                $(item3).addClass('category_selected');
+                                $('.pub-maininfo__selected-categories').append('<span class="pub-maininfo__selected-category">' + $(item1).text() + '</span>')
                             }
                         })
                     }
@@ -586,14 +586,14 @@ $(function() {
                         $(item).removeClass('category_selected');
                         $('.pub-maininfo__category-input_main-category option').each((j, item1) => {
                             $('.pub-maininfo__category-secoundary-inputs-container input[type="hidden"]').each((k, item3) => {
-                                if($(item1).val() == $(item3).val()) {
+                                if ($(item1).val() == $(item3).val()) {
                                     console.log($(item1).val(), $(item3).val())
                                     $(item3).val('');
                                     selectedCount--;
                                 }
                             })
                         });
-                        
+
                         $(e.target).remove();
                     }
                 });
@@ -601,7 +601,7 @@ $(function() {
             $('.pub-maininfo__category-secoundary-inputs-container').find('input[id="category' + String(selectedCount) + '"]').val('');
             selectedCount--;
         }
-       
+
     });
 
 
@@ -1021,6 +1021,20 @@ $(function() {
     });
 
     //MODALS END
+
+    // MAP
+
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(55.014048, 82.913005);
+        var myOptions = {
+            zoom: 8,
+            center: myLatlng
+        }
+        var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    }
+
+    initialize();
+    // MAP END
 });
 
 function sendForm(form, url) {
